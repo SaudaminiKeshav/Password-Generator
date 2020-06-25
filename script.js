daf0179c3d9b0cb0382bed01209cce63a60ec03c
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-06-25 16:53:05 -0700

    Revert "    // Add classes to style dialog in bootstrap"
    This reverts commit 9be3c7014e05fcb0a1b7878712fe57b2b30ac6bf.

--- script.js
+++ script.js
@@ -12,23 +12,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Generates random password 
-function generatePassword(){
-  customPromptGenerator()
-  
-}
-
-function customPromptGenerator(){
-
-  // Get the div in which the dialog will be displayed 
-  var card = document.querySelector(".card-body");
-
-  // Get the textarea that has to be replaced with the welcome dialog
-    var textArea = document.querySelector("textarea");
-
-  // Create the elements to be displayed in the welcome dialog 
-    var welcomeDialogDiv = document.createElement("div");
-    var welcomeDialogHeading = document.createElement("h4");
-    var welcomeDialogBody = document.createElement("p");
-}
